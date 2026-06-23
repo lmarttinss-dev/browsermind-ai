a13408456921434072f26b035172c4b1a62541c5
lmarttinss-dev <lmarttinss.dev@gmail.com>
2026-06-23 15:34:26 -0300

    fix: corrigir select de template que não mantinha seleção visível
    O value do select estava hardcoded como '' em vez de usar activeTemplate,
    fazendo com que o dropdown sempre mostrasse 'Selecionar template de prompt...'

--- webapp/src/components/ChatPanel.tsx
+++ webapp/src/components/ChatPanel.tsx
@@ -123,7 +123,7 @@ export function ChatPanel() {
         {/* Template selector */}
         <div className="relative">
           <select
-            value=""
+            value={activeTemplate}
             onChange={(e) => applyTemplate(e.target.value)}
             className="w-full appearance-none bg-gray-50 border border-gray-200 rounded-lg px-3 py-2 pr-8 text-sm text-gray-500 focus:outline-none focus:ring-2 focus:ring-primary-500 cursor-pointer"
           >
